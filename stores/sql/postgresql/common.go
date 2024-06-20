8c869ccd5d02dd659d36f0cb007b7a8be7f63917
Todd T. Fries <todd@fries.net>
2024-06-20 16:54:33 -0500

    lets be closer to the mysql implementation

--- stores/sql/postgresql/common.go
+++ stores/sql/postgresql/common.go
@@ -1,42 +1,42 @@
-package postgres
+package mysql
 
 import (
-    "context"
-    "embed"
-    "fmt"
+	"context"
+	"embed"
+	"fmt"
 
-    "github.com/jackc/pgx/v5"
+	"go.sia.tech/renterd/internal/sql"
 )
 
 var deadlockMsgs = []string{
-    "deadlock detected",
+	"Deadlock found when trying to get lock",
 }
 
 //go:embed all:migrations/*
 var migrationsFs embed.FS
 
-func applyMigration(ctx context.Context, db *pgx.Conn, fn func(tx pgx.Tx) (bool, error)) error {
-    return db.Begin(ctx, func(tx pgx.Tx) error {
-        _, err := fn(tx)
-        return err
-    })
+func applyMigration(ctx context.Context, db *sql.DB, fn func(tx sql.Tx) (bool, error)) error {
+	return db.Transaction(ctx, func(tx sql.Tx) error {
+		_, err := fn(tx)
+		return err
+	})
 }
 
-func createMigrationTable(ctx context.Context, db *pgx.Conn) error {
-    if _, err := db.Exec(ctx, `
-            CREATE TABLE IF NOT EXISTS migrations (
-                id varchar(255) NOT NULL,
-                PRIMARY KEY (id)
-            );`); err != nil {
-        return fmt.Errorf("failed to create migrations table: %w", err)
-    }
-    return nil
+func createMigrationTable(ctx context.Context, db *sql.DB) error {
+	if _, err := db.Exec(ctx, `
+		CREATE TABLE IF NOT EXISTS migrations (
+			id varchar(255) NOT NULL,
+			PRIMARY KEY (id)
+		);`); err != nil {
+		return fmt.Errorf("failed to create migrations table: %w", err)
+	}
+	return nil
 }
 
-func version(ctx context.Context, db *pgx.Conn) (string, string, error) {
-    var version string
-    if err := db.QueryRow(ctx, "select version()").Scan(&version); err != nil {
-        return "", "", err
-    }
-    return "PostgreSQL", version, nil
+func version(ctx context.Context, db *sql.DB) (string, string, error) {
+	var version string
+	if err := db.QueryRow(ctx, "select version()").Scan(&version); err != nil {
+		return "", "", err
+	}
+	return "PostgreSQL", version, nil
 }
